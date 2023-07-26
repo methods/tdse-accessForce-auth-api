47fa565b0e59c4dc4cc2708a987a00844236cb06
Pira Tejasakulsin <47509562+piratejas@users.noreply.github.com>
2023-07-26 17:59:15 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from controller import authorise
+
+app = Flask(__name__)
+
+app.register_blueprint(authorise, url_prefix="/authorise")
+
+if __name__ == '__main__':
+   app.run(port=5000)
---
+++ controller.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify
+
+authorise = Blueprint("authorise", __name__)
+
+@authorise.route("/", methods=["GET"])
+def get_api_key():
+    try:
+        return jsonify({"API_KEY": "THIS_IS_THE_API_KEY"}), 200
+    except Exception:
+        return jsonify({"Error": "Internal server error"}), 500
