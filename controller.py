3641bd9227d814ecf7c5989ab7759bca024397b6
piratejas <piratejas@gmail.com>
2023-07-27 15:41:37 +0100

    feat: added post route to return jwt

--- controller.py
+++ controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+import jwt
+
 
 authorise = Blueprint("authorise", __name__)
 
@@ -7,5 +9,16 @@ authorise = Blueprint("authorise", __name__)
 def get_api_key():
     try:
         return jsonify({"API_KEY": "THIS_IS_THE_API_KEY"}), 200
-    except Exception:
-        return jsonify({"Error": "Internal server error"}), 500
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
+
+
+@authorise.route("/", methods=["POST"])
+def get_jwt():
+    try:
+        secret = "THIS_IS_A_SECRET"
+        payload = request.json
+        token = jwt.encode(payload=payload, key=secret)
+        return jsonify({"jwt": token}), 200
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
