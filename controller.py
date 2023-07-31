f660b971e20e84ff7206f24504a9cb9a295aa3ec
Lakorthus <julio28ve@gmail.com>
2023-07-31 10:44:37 +0100

    refactor: route with jwt

--- controller.py
+++ controller.py
@@ -1,11 +1,30 @@
 from flask import Blueprint, jsonify
+import jwt
+import datetime
 
 authorise = Blueprint("authorise", __name__)
 
+# Replace "THIS_IS_THE_API_KEY"with your actual secret key
+SECRET_KEY = "THIS_IS_THE_API_KEY"
 
+# Function to generate JWT token
+def generate_token(user_info):
+    payload = {
+        "user_info": user_info,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1)  # Token expires in 1 day
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    return token
+
+# Updated route to return JWT token
 @authorise.route("/", methods=["GET"])
-def get_api_key():
+def get_jwt_token():
     try:
-        return jsonify({"API_KEY": "THIS_IS_THE_API_KEY"}), 200
+        user_info = {
+            "user_id": 123,
+            "scope": "write"  # Add user's scope/permission information here
+        }
+        token = generate_token(user_info)
+        return jsonify({"token": token}), 200
     except Exception:
         return jsonify({"Error": "Internal server error"}), 500
